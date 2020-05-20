3dc44206ffeb30086a793a2980519383b4361ffc
suzumori <you@example.com>
2020-05-20 18:45:10 +0900

    local_settings.pyno

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ PROJECT_NAME = os.path.basename(BASE_DIR)
 SECRET_KEY = '=b&fr%+vmq(!5wluands@e8h!8q=@hixd=ewv$mktsktf&dn40'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -78,12 +78,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
         'USER': 'postgres',
-        'PASSWORD': 'admindjango0317',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
